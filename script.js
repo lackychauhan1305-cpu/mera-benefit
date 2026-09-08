7ab163dd8d21bdf5eda22fc89d7c60d98f8a27f1
lackychauhan1305-cpu <lackychauhan1305@gmail.com>
2026-09-08 11:29:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function startSearch() {
+    alert(
+        "Mera Benefit is getting ready! 🚀\n\n" +
+        "Soon you will answer a few simple questions " +
+        "and discover government schemes and scholarships " +
+        "you may be eligible for."
+    );
+}
